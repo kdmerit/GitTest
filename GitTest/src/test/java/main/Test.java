e609f0966034af036652754590e920e94ada030e
zetamind <kdmerit@gmail.com>
2023-09-06 13:42:10 +0900

    "!2121212

--- GitTest/src/test/java/main/Test.java
+++ GitTest/src/test/java/main/Test.java
@@ -4,13 +4,4 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("aaaaa");
 	}
-
-	public void naora143() {
-		String aaa = "143";
-	}
-	
-	private int naora144() {
-		
-		return 0;
-	}
 }
