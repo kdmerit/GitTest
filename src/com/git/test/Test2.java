f4ec80155dcf6124efe0ae0aa3725be21121bee2
zetamind <kdmerit@gmail.com>
2023-09-12 14:42:49 +0900

    main

--- src/com/git/test/Test2.java
+++ src/com/git/test/Test2.java
@@ -3,5 +3,15 @@ package com.git.test;
 public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("bbbb");
+		
+		
+		
+		
+		System.out.println("main");
+	}
+	
+	
+	public Test2() {
+		// TODO Auto-generated constructor stub  -0> main
 	}
 }
