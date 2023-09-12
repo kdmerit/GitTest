e38db9323259f89d6dae63ce3a10b58e1a8f5d3b
zetamind <kdmerit@gmail.com>
2023-09-12 14:51:43 +0900

    139

--- src/com/git/test/GitTest.java
+++ src/com/git/test/GitTest.java
@@ -3,10 +3,6 @@ package com.git.test;
 public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("Git main");
-		
-		/*
-		 * 143을 나름대로 수정한다.
-		 */
-		System.out.println(143);
+		System.out.println("139");
 	}
 }
